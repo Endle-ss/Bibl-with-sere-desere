ce6eb455999c6f8c31061d1a8f54ff8c51f91152
Koiru <141869441+Endle-ss@users.noreply.github.com>
2024-06-01 13:18:32 +0300

    Add files via upload

---
+++ SerializationLibrary/SerializationLibrary/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SerializationLibrary
+{
+    public class Serializer
+    {
+        public async Task SerializeAsync<T>(T data, string filePath)
+        {
+            try
+            {
+                using (FileStream createStream = File.Create(filePath))
+                {
+                    await JsonSerializer.SerializeAsync(createStream, data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Serialization error: {ex.Message}");
+            }
+        }
+
+        public async Task<T> DeserializeAsync<T>(string filePath)
+        {
+            try
+            {
+                using (FileStream openStream = File.OpenRead(filePath))
+                {
+                    return await JsonSerializer.DeserializeAsync<T>(openStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Deserialization error: {ex.Message}");
+                return default;
+            }
+        }
+    }
+}
---
+++ WpfAppWithSerialization/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SerializationLibrary;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppWithSerialization
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Serializer _serializer;
+        private const string FilePath = "data.json";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _serializer = new Serializer();
+        }
+
+        private async void SerializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var data = InputTextBox.Text;
+            await _serializer.SerializeAsync(data, FilePath);
+            MessageBox.Show("Data serialized!");
+        }
+
+        private async void DeserializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var data = await _serializer.DeserializeAsync<string>(FilePath);
+            OutputTextBox.Text = data ?? "No data found";
+            MessageBox.Show("Data deserialized!");
+        }
+    }
+}
